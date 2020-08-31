5049c88f0516f65d57492d1d632dbbde5cfa5777
Yu Dunaev <dun43v@gmail.com>
2020-08-31 10:54:30 +0300

    2020-08-31 07:54:30

---
+++ README.md
@@ -0,0 +1,16 @@
+# Information
+
+Шаблон дополнения для [**XenForo**](https://xenforo.com/).
+
+## Install
+
+1. Загрузить информацию из **app/upload/**, сохраняя структуру директорий.
+2. Зайти в **AdminCP**, далее *Add-ons*, и нажать кнопку <kbd>Install</kbd> у необходимого дополнения.
+
+## Update
+
+
+
+## Donations
+
+- [Donation Form](https://donation-form.github.io/)
---
+++ Setup.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace EXT\Sample;
+
+use XF\AddOn\AbstractSetup;
+
+class Setup extends AbstractSetup {
+	public function install( array $stepParams = [] ) {
+		// TODO: Implement install() method.
+	}
+
+	public function upgrade( array $stepParams = [] ) {
+		// TODO: Implement upgrade() method.
+	}
+
+	public function uninstall( array $stepParams = [] ) {
+		// TODO: Implement uninstall() method.
+	}
+}
